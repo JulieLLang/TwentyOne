ee2b7e54c7956170ccbc24d33554e58bb635c8a6
Julie Lang <j_lang1@icloud.com>
2019-04-26 15:16:38 -0700

    Initial commit

---
+++ Casino/FraudException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class FraudException : Exception
+    {
+        // 2 constructors one overloads the other
+        public FraudException()
+            : base() { }
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
---
+++ TwentyOne/ExceptionEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class ExceptionEntity
+    {
+        //Entity refers to database object, class maps to a database
+        public int Id { get; set; }
+        public string ExceptionType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
